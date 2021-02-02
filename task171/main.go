9e00b2c480b192af5e0c799d84bf65206cb92bca
Andrey V. Romanchev <andrey.romanchev@gmail.com>
2021-02-02 22:30:01 +0300

    task171

---
+++ task171/main.go
@@ -0,0 +1,16 @@
+package main
+
+type products struct {
+	id         int32
+	name       string
+	categoryId int32
+}
+
+type categories struct {
+	id   int32
+	name int32
+	path int32
+}
+
+func main() {
+}
